c2463c1ee0b087f2e87c3617e69c8da7a71963cf
tiemaocsdn <864927717@qq.com>
2015-07-27 11:11:21 +0800

    新闻详细页

--- app/src/main/java/org/zreo/cnbetareader/Activitys/MainActivity.java
+++ app/src/main/java/org/zreo/cnbetareader/Activitys/MainActivity.java
@@ -1,5 +1,6 @@
 package org.zreo.cnbetareader.Activitys;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
@@ -62,6 +63,7 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this, "你点击了 " + position, Toast.LENGTH_SHORT).show();
+
             }
         });
     }

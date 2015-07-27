c68803911649f85504cdb3e90da18b29b2d35aee
Tingreven.github.io <582865333@qq.com>
2015-07-27 11:35:51 +0800

    咨询主题

--- app/src/main/java/org/zreo/cnbetareader/Activitys/Information_ThemeActivity.java
+++ app/src/main/java/org/zreo/cnbetareader/Activitys/Information_ThemeActivity.java
@@ -1,55 +1,179 @@
 package org.zreo.cnbetareader.Activitys;
+import java.util.ArrayList;
 
-import android.app.TabActivity;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
-import android.widget.TabHost;
+import android.app.Activity;
+
+import android.graphics.Color;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+
+
+import android.view.LayoutInflater;
+
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+
+import android.widget.TextView;
 
 import org.zreo.cnbetareader.R;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Admin on 2015/7/25.
  */
-public class Information_ThemeActivity extends TabActivity {
+public class Information_ThemeActivity extends Activity  implements OnClickListener{
+    ViewPager viewPager;
+    int position;
+    ViewPager ipager = null;
+    PagerTabStrip itabStrip = null;
+    ArrayList<View> viewContainter = new ArrayList<View>();
+    ArrayList<String> titleContainer = new ArrayList<String>();
+    public String TAG = "tag";
+    private ViewPager viewpager;
+    private PagerAdapter iAdapter;  //ViewPager的适配器
+    private ArrayList<View> views;
+    private View view1;
+    private View view2;
+
+
+    private TextView itv_Tab1;
+    private TextView itv_Tab2;
+    private int currentIndex = 0;  //当前标签页
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.information_theme);
-        //设置标签的名称
-        TabHost itabHost=(TabHost)findViewById(android.R.id.tabhost);
-        TabHost.TabSpec itab1=itabHost.newTabSpec("itab1");
-        itab1.setIndicator("已关注");
-        itab1.setContent(R.id.tab_1);
-        itabHost.addTab(itab1);
+        init();
 
-        TabHost.TabSpec itab2=itabHost.newTabSpec("itab2");
-        itab2.setIndicator("可关注");
-         itab2.setContent(R.id.tab_2);
-        itabHost.addTab(itab2);
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
+        viewpager.setCurrentItem(0);  //默认页面
+        itv_Tab1.setTextColor(Color.BLUE);
+        itv_Tab2.setTextColor(Color.BLUE);
+        viewpager.setAdapter(iAdapter);
+        viewpager.setOnPageChangeListener(new OnPageChangeListener()
+        {
+            @Override
+            public void onPageSelected(int position)
+            {
+                initColor();
+                switch (position)
+                {
+                    case 0:
+                        itv_Tab1.setTextColor(Color.WHITE);
+                        break;
+                    case 1:
+                        itv_Tab2.setTextColor(Color.WHITE);
+                        break;
+                }
+                currentIndex = position;
+            }
 
-        ListView information_lv1=(ListView)findViewById(R.id.information_lv1);
-        //ListView information_lv2=(ListView)findViewById(R.id.information_lv2);
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2)
+            {
+            }
 
-        List<Map<String,Object>> idata=new ArrayList<Map<String,Object>>();
-        Map<String,Object>map1=new HashMap<String,Object>();
-        Map<String,Object>map2=new HashMap<String,Object>();
+            @Override
+            public void onPageScrollStateChanged(int arg0)
+            {
+            }
+        });
+    }
 
-        map2.put("photo",R.drawable.menubtn);
-        map2.put("name","测试ListView");
-        idata.add(map2);
+    //初始化TestView文本颜色
 
-        information_lv1.setAdapter(new SimpleAdapter(this,idata,R.layout.information_theme,new String[]{"photo","name"}
-                ,new int[]{R.id.information_imageview,R.id.information_tv}));
+    public void initColor() {
+        itv_Tab1.setTextColor(Color.BLUE);
+        itv_Tab2.setTextColor(Color.BLUE);
+
+    }
+
+    //初始主要的显示内容。并且加入组件中
+    public void init(){
+        itv_Tab1 = (TextView) findViewById(R.id. itv_Tab1);
+        itv_Tab2 = (TextView) findViewById(R.id. itv_Tab2);
+
+        itv_Tab1.setOnClickListener((OnClickListener) this);
+        itv_Tab2.setOnClickListener((OnClickListener) this);
 
 
+        views = new ArrayList<View>();
+        LayoutInflater f = getLayoutInflater();
+        view1 = f.inflate(R.layout. itv_tab1, null);
+        view2 = f.inflate(R.layout.itv_tab2, null);
+
+        views.add(view1);
+        views.add(view2);
+
+
+        iAdapter = new PagerAdapter()
+        {
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object)
+            {
+                container.removeView(views.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position)
+            {
+                View view = views.get(position);
+                container.addView(view);
+                return view;
+            }
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1)
+            {
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount()
+            {
+                return views.size();
+            }
+        };
 
 
 
     }
+
+    public void onClick(View v) {
+        //调用arrowScroll方法用参数1或者17就可以实现向左翻页；参数2或66就可以实现向右翻页。
+        //当你的UI中有EditText这种获得focus的widget时，则必须用17和66，否则要报错。
+        int FOCUS_LEFT = 17;
+        int FOCUS_RIGHT = 66;
+        switch (v.getId()) {
+            case R.id. itv_Tab1:
+                if (currentIndex == 0) {	//不跳转
+                } else if (currentIndex == 1) {
+                    viewpager.arrowScroll(FOCUS_LEFT);
+                } else if (currentIndex == 2) {
+                    viewpager.arrowScroll(FOCUS_LEFT);
+                    viewpager.arrowScroll(FOCUS_LEFT);
+                }
+                break;
+            case R.id. itv_Tab2:
+                if (currentIndex == 0) {
+                    viewpager.arrowScroll(FOCUS_RIGHT);
+                } else if (currentIndex == 1) {		//不跳转
+                } else if (currentIndex == 2) {
+                    viewpager.arrowScroll(FOCUS_LEFT);
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+
 }

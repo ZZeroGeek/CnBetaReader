c3ce4dedccb6998e8fb88b995e1f4a26cfad905c
linJZhong <675795202@qq.com>
2015-08-06 21:19:46 +0800

    显示缓存大小

--- app/src/main/java/org/zreo/cnbetareader/Fragments/SettingFragment.java
+++ app/src/main/java/org/zreo/cnbetareader/Fragments/SettingFragment.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AlertDialog;
@@ -11,27 +14,33 @@ import android.text.format.Formatter;
 import android.widget.Toast;
 import android.text.format.Formatter;
 
-
 import com.nostra13.universalimageloader.core.ImageLoader;
 import org.zreo.cnbetareader.Activitys.MainActivity;
+import org.zreo.cnbetareader.MyApplication;
 import org.zreo.cnbetareader.R;
 import org.zreo.cnbetareader.Utils.FileKit;
 import org.zreo.cnbetareader.Utils.ThemeManger;
 
 import java.io.File;
+import java.math.BigDecimal;
 
 
 public class SettingFragment extends PreferenceFragment {
 
-    private boolean running = false;
+
     private int themeid;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pre_setting);
+        Preference   preference = findPreference(getString(R.string.clean_cache_key));
+
+        preference.setSummary(getFileSize());
         Preference theme = findPreference("theme");
         theme.setOnPreferenceClickListener(onPreferenceClickListener);
+        findPreference(getString(R.string.clean_cache_key)).setOnPreferenceClickListener(onPreferenceClickListener);
+        //clean_cache_key.setOnPreferenceClickListener(onPreferenceClickListener);
     }
 
     Preference.OnPreferenceClickListener onPreferenceClickListener = new Preference.OnPreferenceClickListener() {
@@ -56,9 +65,13 @@ public class SettingFragment extends PreferenceFragment {
 
             }
             return false;
-        }
+        }};
+    private String getFileSize() {
+        long size = 0;
+        size += FileKit.getFolderSize("data/data/org.zreo.cnbetareader/cache");
+        return Formatter.formatFileSize(getActivity(), size);
+    }
 
-    };
 }
 
 
